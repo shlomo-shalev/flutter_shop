20b683154d59690a835071042af6ddbc01519895
yeledkesem <proyektym@gmail.com>
2022-04-30 15:51:35 +0300

    start

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
