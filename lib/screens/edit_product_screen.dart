06e55b8844786269441407cc54f58b6a1699fdbb
yeledkesem <proyektym@gmail.com>
2022-06-07 14:42:07 +0300

    section nine episode two hundred and thirty-one

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,8 @@
 // packeges
+import 'dart:developer';
 import 'package:flutter/material.dart';
+// providers
+import 'package:flutter_shop_app/providers/product_provider.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const String routeName = '/edit-product';
@@ -11,23 +14,218 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final FocusNode _priceFocusNode = FocusNode();
+  final FocusNode _descriptionFocusNode = FocusNode();
+  final FocusNode _imageUrlFocusNode = FocusNode();
+  final TextEditingController _imageUrlController = TextEditingController();
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+  ProductProvider _product = ProductProvider(
+    id: '',
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: '',
+  );
+  bool autoAlways = false;
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_chnageFocusForImageUrl);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _imageUrlFocusNode.removeListener(_chnageFocusForImageUrl);
+
+    _priceFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+
+    _imageUrlController.dispose();
+    super.dispose();
+  }
+
+  void _chnageFocusForImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  void _saveForm() {
+    setState(() {
+      autoAlways = true;
+    });
+    if (!_form.currentState!.validate()) return;
+    _form.currentState!.save();
+    inspect(_product);
+  }
+
+  void _addFeild(name, value) {
+    _replaceData(isReplace, newValue, oldValue) {
+      return isReplace ? newValue : oldValue;
+    }
+
+    var id = _replaceData(name == 'id', value, _product.id);
+    var title = _replaceData(name == 'title', value, _product.title);
+    var description =
+        _replaceData(name == 'description', value, _product.description);
+    var price = _replaceData(name == 'price', value, _product.price);
+    var imageUrl = _replaceData(name == 'imageUrl', value, _product.imageUrl);
+    _product = ProductProvider(
+      id: id,
+      title: title,
+      description: description,
+      price: price,
+      imageUrl: imageUrl,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit product'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: _saveForm,
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          autovalidateMode: autoAlways ? AutovalidateMode.always : null,
+          key: _form,
           child: ListView(
             children: <Widget>[
               TextFormField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'title',
+                  labelText: 'Title',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Title is must!';
+                  }
+                  return null;
+                },
+                textInputAction: TextInputAction.next,
+                autofocus: true,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                onSaved: (value) => _addFeild('title', value),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Amount',
                 ),
                 textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Amount is must!';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Amount is not Valid!';
+                  }
+                  if (double.parse(value) <= 0) {
+                    return 'Amount does not bigger 0.';
+                  }
+                  return null;
+                },
+                onSaved: (value) =>
+                    _addFeild('price', double.parse(value as String)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Description',
+                ),
+                maxLines: 3,
+                textInputAction: TextInputAction.newline,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+                onSaved: (value) => _addFeild('description', value),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Description is must!';
+                  }
+                  if (value.length < 10) {
+                    return 'Description is smaller 10';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? const Text('Ender a image Url')
+                        : Image.network(
+                            _imageUrlController.text,
+                            fit: BoxFit.cover,
+                          ),
+                    margin: const EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Image URL',
+                      ),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.url,
+                      focusNode: _imageUrlFocusNode,
+                      controller: _imageUrlController,
+                      onSaved: (value) => _addFeild('imageUrl', value),
+                      onFieldSubmitted: (_) => _saveForm(),
+                      // onEditingComplete: () {
+                      //   _imageUrlFocusNode.unfocus();
+                      // },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Image URL is must!';
+                        }
+                        if (!value.startsWith('http') ||
+                            !value.startsWith('https')) {
+                          return 'Image URL is not valid';
+                        }
+                        if (!value.endsWith('.png') &&
+                            !value.startsWith('.jpg') &&
+                            !value.startsWith('.jpeg')) {
+                          return 'Image URL is not valid';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 // providers
 import 'package:flutter_shop_app/providers/product_provider.dart';
 import 'package:flutter_shop_app/providers/products_provider.dart';
+// screens
+import 'package:flutter_shop_app/screens/edit_product_screen.dart';
 // widgets
 import 'package:flutter_shop_app/widgets/app_drawer.dart';
 import 'package:flutter_shop_app/widgets/user_product_block.dart';
@@ -23,7 +25,9 @@ class UserProductsScreen extends StatelessWidget {
         title: const Text('Your prodcuts'),
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
             icon: const Icon(Icons.add),
           ),
         ],
